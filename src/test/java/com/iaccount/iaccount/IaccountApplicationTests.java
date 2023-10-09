422531d4443c0dedb2c02732584a7d7babe81608
kishorkumbhar <k.k.kumbhar007@hotmail.com>
2023-10-10 05:16:23 +0530

    added files

---
+++ src/test/java/com/iaccount/iaccount/IaccountApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iaccount.iaccount;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IaccountApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
